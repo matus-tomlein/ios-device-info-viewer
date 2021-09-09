d14688050969f377b144eb400137f57ed7d1d56b
Matúš Tomlein <matus.tomlein@gmail.com>
2021-09-09 12:34:55 +0200

    Initial Commit

---
+++ DeviceInfoViewer/DeviceInfoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceInfoViewerApp.swift
+//  DeviceInfoViewer
+//
+//  Created by Matus Tomlein on 09/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct DeviceInfoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
